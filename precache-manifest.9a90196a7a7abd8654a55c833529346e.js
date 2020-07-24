27c1912890e6693c2e0d9de9246573da10295625
Ibrahim Shaker <shaker.personnel@gmail.com>
2020-07-24 04:58:19 +0200

    Updates

---
+++ precache-manifest.9a90196a7a7abd8654a55c833529346e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f176ab5654c0284e5a4b6682ad93ee1e",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "99e933214dc38aa355a9",
+    "url": "/todolist/static/css/main.43e5afd9.chunk.css"
+  },
+  {
+    "revision": "30d1d5cb4e5a5bd0e52a",
+    "url": "/todolist/static/js/2.1ba60410.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todolist/static/js/2.1ba60410.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99e933214dc38aa355a9",
+    "url": "/todolist/static/js/main.f04088fb.chunk.js"
+  },
+  {
+    "revision": "a5304b855d42e1b78d29",
+    "url": "/todolist/static/js/runtime-main.27cecdba.js"
+  }
+]);
